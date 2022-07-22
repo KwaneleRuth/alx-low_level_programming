db55dd97c654f7d3389a715d2e101ac322531485
KwaneleRuth <kwanelerndlovu@gmail.com>
2022-07-22 02:29:20 -0700

    correction of betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,42 +10,42 @@
 
 int main(int argc, char **argv)
 {
-  int sum, n;
+int sum, n;
 
-  if (argc != 2)
-    {
-      printf("Error\n");
-      return (1);
-    }
-  sum = atoi(argv[1]);
-  while (sum > 0)
-    {
-      if (sum >= 25)
-	{
-	  n++;
-	  sum = sum - 25;
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+sum = atoi(argv[1]);
+while (sum > 0)
+{
+if (sum >= 25)
+{
+n++;
+sum = sum - 25;
 	}
-      else if (sum >= 10)
-	{
-	  n++;
-	  sum = sum - 10;
-	}
-      else if (sum >= 5)
-	{
-	  n++;
-	  sum = sum - 5;
-	}
-      else if (sum >= 2)
-	{
-	  n++;
-	  sum = sum - 2;
-	}
-      else if (sum >= 1)
-	{
-	  n++;
-	  sum = sum - 1;
-	}
-    }
-  printf("%d\n", n);
-  return (0);
+else if (sum >= 10)
+{
+n++;
+sum = sum - 10;
+}
+else if (sum >= 5)
+{
+n++;
+sum = sum - 5;
+}
+else if (sum >= 2)
+{
+n++;
+sum = sum - 2;
+}
+else if (sum >= 1)
+{
+n++;
+sum = sum - 1;
+}
+}
+printf("%d\n", n);
+return (0);
 }
